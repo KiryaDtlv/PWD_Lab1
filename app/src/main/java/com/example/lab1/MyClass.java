091b3461a73cdb38c13d261a3603a2a4049dfdeb
KiryaDtlv <dyatlov.kiryuha@gmail.com>
2022-09-13 16:53:42 +0700

    generated javadoc

--- app/src/main/java/com/example/lab1/MyClass.java
+++ app/src/main/java/com/example/lab1/MyClass.java
@@ -1,6 +1,16 @@
 package com.example.lab1;
 
+/**
+ * Класс продукции со свойствами <b>maker</b> и <b>price</b>.
+ * @autor Кирилл Дятлов, Артём Макаров
+ * @version 1.0
+ */
 public class MyClass {
+    /**
+     * Функция получения минимального значения из массива
+     * @param mas - массив значений
+     * @return возвращает минимальное значение массива
+     */
     public static int min(int[] mas) {
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < mas.length; i++) {
@@ -8,7 +18,11 @@ public class MyClass {
         }
         return min;
     }
-
+    /**
+     * Функция получения максимального значения из массива
+     * @param mas - массив значений
+     * @return возвращает максимальное значение массива
+     */
     public static int max(int[] mas) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < mas.length; i++) {
